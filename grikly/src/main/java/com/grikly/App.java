d8ef052ccff89c2b86020e946c0eee3ecfe00f62
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-11 16:11:05 -0500

    Change to fetch Card by Id

--- grikly/src/main/java/com/grikly/App.java
+++ grikly/src/main/java/com/grikly/App.java
@@ -1,7 +1,6 @@
 package com.grikly;
 
 import com.grikly.model.Card;
-import com.grikly.model.User;
 
 
 public class App 
@@ -9,26 +8,15 @@ public class App
 	public App() 
 	{
 		Grikly grikly = new Grikly("C049DBE3-C9E6-4176-9984-27C0119DB2D0");
-		/*grikly.fetchCard(1, new ResponseListener<Card>() {
+		grikly.fetchCard(1, new ResponseListener<Card>() {
 			
 			public void response(Card result) {
 				System.out.println(result);
 				
 			}
 		});
-		*/
-		Card card = new Card();
-		card.setFirstName("Mario");
-		card.setLastName("Dennis");
-		card.setEmail("mdennis876@gmail.com");
 		
-		grikly.createCard(card, new ResponseListener<Card>() {
-			
-			public void response(Card result) {
-				System.out.println(result);
-				
-			}
-		});
+	
 	}
 	
     public static void main( String[] args )
