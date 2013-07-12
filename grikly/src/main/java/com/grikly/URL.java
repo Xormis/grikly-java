fe5dd90b5e07451ac7eaefeba4657dbc669d31b1
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-12 09:38:30 -0500

    Changed CARD_BASE url, which fixed Http 405 error that createCard method of Grikly class was returning.

--- grikly/src/main/java/com/grikly/URL.java
+++ grikly/src/main/java/com/grikly/URL.java
@@ -5,8 +5,9 @@ public enum URL {
 	ACCOUNT_REGISTER ("http://api.grik.ly/v1/Account/Register"),
 	ACCOUNT_EMAIL_CHECK ("http://api.grik.ly/v1/Account/EmailExist"),
 	ACCOUNT_LOGIN("http://api.grik.ly/v1/Account/Login"),
+	ACCOUNT("http://api.grik.ly/v1/Account/%s"),
 	USER ("http://api.grik.ly/v1/Users/%d"),
-	CARD_BASE ("http://api.grik.ly/v1/Users/"),
+	CARD_BASE ("http://api.grik.ly/v1/Cards/"),
 	CARD ("http://api.grik.ly/v1/Cards/%d");
 	
 	private final String url;
