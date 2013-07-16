e562aee752a2518de07ebc05ebe4b0d9d61dbffb
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-16 13:59:36 -0500

    Add Comments

--- grikly/src/main/java/com/grikly/request/HttpPostRequest.java
+++ grikly/src/main/java/com/grikly/request/HttpPostRequest.java
@@ -8,9 +8,22 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-
+/**
+ * HttpPostRequest is used to execute a HTTP
+ * Request using the HTTP POST method. This class 
+ * is thread-safe and cannot be sub-classed.
+ * @author Mario Dennis
+ *
+ * @param <E>
+ * @param <T>
+ */
 public final class HttpPostRequest <E,T> extends HttpRequest<E, T>{
 	
+	/**
+	 * HttpPostRequest Default Constructor.
+	 * @author Mario Dennis
+	 * @param HttpBuilder<E, T>
+	 */
 	protected HttpPostRequest(HttpBuilder<E, T> builder)
 	{
 		super(builder);
@@ -20,6 +33,7 @@ public final class HttpPostRequest <E,T> extends HttpRequest<E, T>{
 	/**
 	 * Execute HTTP POST request to Grikly Server.
 	 * @author Mario Dennis
+	 * @return T
 	 */
 	public T execute() 
 	{
@@ -45,7 +59,6 @@ public final class HttpPostRequest <E,T> extends HttpRequest<E, T>{
 		
 		else 
 			response = resource.header("ApiKey", getApiKey())
-							   .header("Authorization","Basic " + getAuthInfo())
 							   .type(MediaType.APPLICATION_JSON)
 							   .accept(MediaType.APPLICATION_JSON)
 							   .post(ClientResponse.class,gson.toJson(getModel()));
