e222b85c74c218fb7954a19f5f5e43a1cf468cc4
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-16 13:47:21 -0500

    Add Comment

--- grikly/src/main/java/com/grikly/request/HttpRequest.java
+++ grikly/src/main/java/com/grikly/request/HttpRequest.java
@@ -2,6 +2,14 @@ package com.grikly.request;
 
 import javax.ws.rs.core.MultivaluedMap;
 
+/**
+ * HttpRequest is a abstract class that contain
+ * all properties that are need to do HTTP Request 
+ * @author mario
+ *
+ * @param <E>
+ * @param <T>
+ */
 public abstract class HttpRequest <E,T> implements IHttpRequest<E, T> {
 	
 	private final String apiKey;
@@ -11,6 +19,11 @@ public abstract class HttpRequest <E,T> implements IHttpRequest<E, T> {
 	private final E model;
 	private final String authedInfo;
 	
+	/**
+	 * HttpRequest Default Constructor
+	 * @author Mario Dennis
+	 * @param HttpBuilder<E,T>
+	 */
 	protected HttpRequest (HttpBuilder<E, T> builder)
 	{
 		this.apiKey = builder.getApiKey();
@@ -32,56 +45,73 @@ public abstract class HttpRequest <E,T> implements IHttpRequest<E, T> {
 	
 	
 	/**
+	 * Get apiKey
+	 * @author Mario Dennis
 	 * @return the apiKey
 	 */
 	public String getApiKey()
 	{
 		return apiKey;
-	}
+	}//end getApiKey method
+	
 	
 
 	/**
+	 * Get type
+	 * @author Mario Dennis
 	 * @return the type
 	 */
 	public Class<T> getType()
 	{
 		return type;
-	}
+	}//end getType method
 
 	
+	
 	/**
+	 * Get path
+	 * @author Mario Dennis
 	 * @return the path
 	 */
 	public String getPath()
 	{
 		return path;
-	}
+	}//end getPath method
+	
+	
 
 	/**
+	 * Get queryMap
+	 * @author Mario Dennis
 	 * @return the queryMap
 	 */
 	public MultivaluedMap<String, String> getQueryMap() 
 	{
 		return queryMap;
-	}
+	}//end getQueryMap method
 
 
 	/**
+	 * Get model
+	 * @author Mario Dennis
 	 * @return the model
 	 */
 	public E getModel() 
 	{
 		return model;
-	}
+	}//end getModel method
 	
 
+	
 	/**
+	 * Get authInfo
+	 * @author Mario Dennis
 	 * @return the authInfo
 	 */
 	public String getAuthInfo ()
 	{
 		return authedInfo;
-	}
+	}//end getAuthInfo method
 
 
 
