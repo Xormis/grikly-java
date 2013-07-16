88569632401e146b3f6ff6e8e7a0789e9b3e4dff
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-16 14:47:25 -0500

    Create Contact Model

---
+++ grikly/src/main/java/com/grikly/model/Contact.java
@@ -0,0 +1,154 @@
+package com.grikly.model;
+
+import java.util.Date;
+
+public class Contact {
+
+	private int CardId;
+	private String EventMet;
+    private String PlaceMet;
+    private double LatitudeMet;
+    private double LongitudeMet;
+    private Date DateMet;
+    private String Comment;
+    
+    
+	/**
+	 * @return the cardId
+	 */
+	public int getCardId() 
+	{
+		return CardId;
+	}
+	
+	
+	/**
+	 * @param cardId the cardId to set
+	 */
+	public void setCardId(int cardId) 
+	{
+		CardId = cardId;
+	}
+	
+	
+	/**
+	 * @return the eventMet
+	 */
+	public String getEventMet() 
+	{
+		return EventMet;
+	}
+	
+	
+	/**
+	 * @param eventMet the eventMet to set
+	 */
+	public void setEventMet(String eventMet) 
+	{
+		EventMet = eventMet;
+	}
+	
+	
+	/**
+	 * @return the placeMet
+	 */
+	public String getPlaceMet() 
+	{
+		return PlaceMet;
+	}
+	
+	
+	/**
+	 * @param placeMet the placeMet to set
+	 */
+	public void setPlaceMet(String placeMet) 
+	{
+		PlaceMet = placeMet;
+	}
+	
+	
+	/**
+	 * @return the latitudeMet
+	 */
+	public double getLatitudeMet() 
+	{
+		return LatitudeMet;
+	}
+	
+	
+	/**
+	 * @param latitudeMet the latitudeMet to set
+	 */
+	public void setLatitudeMet(double latitudeMet)
+	{
+		LatitudeMet = latitudeMet;
+	}
+	
+	
+	/**
+	 * @return the longitudeMet
+	 */
+	public double getLongitudeMet() 
+	{
+		return LongitudeMet;
+	}
+	
+	
+	/**
+	 * @param longitudeMet the longitudeMet to set
+	 */
+	public void setLongitudeMet(double longitudeMet) 
+	{
+		LongitudeMet = longitudeMet;
+	}
+	
+	
+	/**
+	 * @return the dateMet
+	 */
+	public Date getDateMet()
+	{
+		return DateMet;
+	}
+	
+	
+	/**
+	 * @param dateMet the dateMet to set
+	 */
+	public void setDateMet(Date dateMet)
+	{
+		DateMet = dateMet;
+	}
+	
+	
+	/**
+	 * @return the comment
+	 */
+	public String getComment() 
+	{
+		return Comment;
+	}
+	
+	
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment)
+	{
+		Comment = comment;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Contact [CardId=" + CardId + ", EventMet=" + EventMet
+				+ ", PlaceMet=" + PlaceMet + ", LatitudeMet=" + LatitudeMet
+				+ ", LongitudeMet=" + LongitudeMet + ", DateMet=" + DateMet
+				+ ", Comment=" + Comment + "]";
+	}
+    
+    
+}//end Contact class
