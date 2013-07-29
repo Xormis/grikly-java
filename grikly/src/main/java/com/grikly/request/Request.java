377c05bd1f08ba165b19889a1a097ba8cff5676a
Mario Dennis <mario_leif2000@yahoo.com>
2013-07-28 20:29:21 -0500

    Change name of IHttpRequest to Request

---
+++ grikly/src/main/java/com/grikly/request/Request.java
@@ -0,0 +1,20 @@
+package com.grikly.request;
+
+/**
+ * Request Interface that all request class
+ * should implement.
+ * @author Mario Dennis
+ * @param <E> element type for data sending in request 
+ * @param <T> return type.
+ */
+public interface Request<E,T>{
+
+	/**
+	 * Execute a HTTP Request.
+	 * @author Mario Dennis
+	 * @param url
+	 * @return
+	 */
+	public T execute();
+	
+}//end IHttpRequest interface
