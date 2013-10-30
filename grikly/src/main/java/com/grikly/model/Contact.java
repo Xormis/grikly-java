159b320f683f3cbfb065ed0a0c7e3e18682bb217
mdennis876 <mdennis876@gmail.com>
2013-10-30 15:46:22 -0700

    Modify Contact model to contain CardId,UserProfileImageUri,UserId,FirstName,and LastName attributes

--- grikly/src/main/java/com/grikly/model/Contact.java
+++ grikly/src/main/java/com/grikly/model/Contact.java
@@ -3,15 +3,9 @@ package com.grikly.model;
 
 public class Contact {
 	private int CardId;
-	private String EventMet;
-    private String PlaceMet;
-    private double LatitudeMet;
-    private double LongitudeMet;
-    private String DateMet;
-    private String Comment;
+	public int UserId;
     public String UserProfileImageUri;
-    public int UserId;
-    public String FirstName;
+    public String FirstName,LastName;
     
     
     /**
@@ -77,43 +71,6 @@ public class Contact {
 		LastName = lastName;
 	}
 
-
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return Title;
-	}
-
-
-	/**
-	 * @param title the title to set
-	 */
-	public void setTitle(String title) {
-		Title = title;
-	}
-
-
-	/**
-	 * @return the companyName
-	 */
-	public String getCompanyName() {
-		return CompanyName;
-	}
-
-
-	/**
-	 * @param companyName the companyName to set
-	 */
-	public void setCompanyName(String companyName) {
-		CompanyName = companyName;
-	}
-
-
-	public String LastName;
-    public String Title;
-    public String CompanyName;
-
 	/**
 	 * @return the cardId
 	 */
@@ -132,126 +89,18 @@ public class Contact {
 	}
 	
 	
-	/**
-	 * @return the eventMet
-	 */
-	public String getEventMet() 
-	{
-		return EventMet;
-	}
-	
-	
-	/**
-	 * @param eventMet the eventMet to set
-	 */
-	public void setEventMet(String eventMet) 
-	{
-		EventMet = eventMet;
-	}
-	
-	
-	/**
-	 * @return the placeMet
-	 */
-	public String getPlaceMet() 
-	{
-		return PlaceMet;
-	}
-	
-	
-	/**
-	 * @param placeMet the placeMet to set
-	 */
-	public void setPlaceMet(String placeMet) 
-	{
-		PlaceMet = placeMet;
-	}
-	
-	
-	/**
-	 * @return the latitudeMet
-	 */
-	public double getLatitudeMet() 
-	{
-		return LatitudeMet;
-	}
-	
-	
-	/**
-	 * @param latitudeMet the latitudeMet to set
-	 */
-	public void setLatitudeMet(double latitudeMet)
-	{
-		LatitudeMet = latitudeMet;
-	}
-	
-	
-	/**
-	 * @return the longitudeMet
-	 */
-	public double getLongitudeMet() 
-	{
-		return LongitudeMet;
-	}
-	
-	
-	/**
-	 * @param longitudeMet the longitudeMet to set
-	 */
-	public void setLongitudeMet(double longitudeMet) 
-	{
-		LongitudeMet = longitudeMet;
-	}
-	
-	
-	/**
-	 * @return the dateMet
-	 */
-	public String getDateMet()
-	{
-		return DateMet;
-	}
-	
-	
-	/**
-	 * @param dateMet the dateMet to set
-	 */
-	public void setDateMet(String dateMet)
-	{
-		DateMet = dateMet;
-	}
-	
-	
-	/**
-	 * @return the comment
-	 */
-	public String getComment() 
-	{
-		return Comment;
-	}
-	
-	
-	/**
-	 * @param comment the comment to set
-	 */
-	public void setComment(String comment)
-	{
-		Comment = comment;
-	}
-	
-	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "Contact [CardId=" + CardId + ", EventMet=" + EventMet
-				+ ", PlaceMet=" + PlaceMet + ", LatitudeMet=" + LatitudeMet
-				+ ", LongitudeMet=" + LongitudeMet + ", DateMet=" + DateMet
-				+ ", Comment=" + Comment + ", UserProfileImageUri="
-				+ UserProfileImageUri + ", UserId=" + UserId + ", FirstName="
-				+ FirstName + ", LastName=" + LastName + ", Title=" + Title
-				+ ", CompanyName=" + CompanyName + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Contact [CardId=").append(CardId).append(", UserId=")
+				.append(UserId).append(", UserProfileImageUri=")
+				.append(UserProfileImageUri).append(", FirstName=")
+				.append(FirstName).append(", LastName=").append(LastName)
+				.append("]");
+		return builder.toString();
 	}
     
     
